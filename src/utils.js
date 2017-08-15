0469810a15440e4588900370106dc7ce9753927e
mndr <amandolin@ymail.com>
2017-08-15 14:49:57 -0500

    setting up shakespeare variables from API

---
+++ src/utils.js
@@ -0,0 +1,11 @@
+const shakespeareAPI = "http://api.graph.cool/simple/v1/shaekspeare"
+
+let options = {
+  method: "POST",
+  headers: {
+    "Content=Type": "application/json"
+  },
+  body: JSON.stringify({
+    // TBD
+  })
+}
